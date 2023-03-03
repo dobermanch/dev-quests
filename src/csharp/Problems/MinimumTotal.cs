63119d94a16182eb9115336f65bc5df05483bf87
Sergii Chevkota <chevkota@live.com>
2023-03-02 17:13:24 -0700

    120. Triangle

--- src/csharp/Problems/MinimumTotal.cs
+++ src/csharp/Problems/MinimumTotal.cs
@@ -9,47 +9,51 @@ public sealed class MinimumTotal : ProblemBase
     public override void Test(object[] data) => base.Test(data);
 
     public override void AddTestCases()
-        => Add(true, it => it.Param2dArray("[[2],[3,4],[6,5,7],[4,1,8,3]]").Result(11))
+        => AddSolutions(nameof(Solution1))
+          .Add(it => it.Param2dArray("[[2],[3,4],[6,5,7],[4,1,8,3]]").Result(11))
           .Add(true, it => it.Param2dArray("[[2],[3,4],[6,5,4],[4,1,8,3]]").Result(11))
-          .Add(it => it.Param2dArray("[[2],[3,4],[6,5,4],[3,4,3,2]]").Result(12))
-          .Add(it => it.Param2dArray("[[2],[3,4],[6,5,4],[3,4,3,2],[3,1,4,5,2]]").Result(14))
-          .Add(it => it.Param2dArray("[[2],[3,4],[6,5,4],[4,2,4,2],[3,2,4,5,1]]").Result(13))
-          .Add(it => it.Param2dArray("[[2],[3,4],[6,5,4],[4,3,4,2],[3,1,4,5,3]]").Result(14))
-          .Add(it => it.Param2dArray("[[2],[3,4],[6,5,6],[6,4,7,3],[3,1,4,5,3]]").Result(15))
-          .Add(it => it.Param2dArray("[[2],[3,4],[6,5,6],[6,3,7,1],[3,1,4,2,4]]").Result(14))
-          .Add(it => it.Param2dArray("[[-10]]").Result(-10));
+          .Add(true, it => it.Param2dArray("[[2],[3,4],[6,5,4],[3,4,3,2]]").Result(12))
+          .Add(true, it => it.Param2dArray("[[2],[3,4],[6,5,4],[3,4,3,2],[3,1,4,5,2]]").Result(14))
+          .Add(true, it => it.Param2dArray("[[2],[3,4],[6,5,4],[4,2,4,2],[3,2,4,5,1]]").Result(13))
+          .Add(true, it => it.Param2dArray("[[2],[3,4],[6,5,4],[4,3,4,2],[3,1,4,5,3]]").Result(14))
+          .Add(true, it => it.Param2dArray("[[2],[3,4],[6,5,6],[6,4,7,3],[3,1,4,5,3]]").Result(15))
+          .Add(true, it => it.Param2dArray("[[2],[3,4],[6,5,6],[6,3,7,1],[3,1,4,2,4]]").Result(14))
+          .Add(true, it => it.Param2dArray("[[-10]]").Result(-10));
 
     private int Solution(IList<IList<int>> triangle)
     {
-        var count = 0;
-        //var index = 0;
-        //for (var i = 0; i < triangle.Count; i++)
-        //{
-        //    if (triangle[i].Count > index + 1)
-        //    {
-        //        if (triangle[i][index + 1] < triangle[i][index])
-        //        {
-        //            index++;
-        //        }
-        //    }
-
-        //    count += triangle[i][index];
-        //}
-
-        var accum = 0;
-        var nums = new[] { 1, 3, 1, -1, 3 };
-        for (int i = 0; i < nums.Length; i++)
+        for (var i = triangle.Count - 2; i >= 0; i--)
         {
-            //if ((accum & nums[i]) == nums[i])
-            //{
-                accum ^= nums[i];
-            //}
-            //else
-            //{
-            //    accum |= nums[i];
-            //}
+            for (var j = 0; j < triangle[i].Count; j++)
+            {
+                triangle[i][j] += Math.Min(triangle[i + 1][j], triangle[i + 1][j + 1]);
+            }
         }
 
-        return count;
+        return triangle[0][0];
+    }
+
+    private int Solution1(IList<IList<int>> triangle)
+    {
+        for (var i = 0; i < triangle.Count - 1; i++)
+        {
+            var temp = 0;
+            for (int j = 0; j < triangle[i].Count; j++)
+            {
+                if (j > 0)
+                {
+                    triangle[i + 1][j] = Math.Min(temp + triangle[i][j], triangle[i + 1][j]);
+                }
+                else
+                {
+                    triangle[i + 1][j] += triangle[i][j];
+                }
+
+                temp = triangle[i + 1][j + 1];
+                triangle[i + 1][j + 1] += triangle[i][j];
+            }
+        }
+
+        return triangle[^1].Min();
     }
 }
