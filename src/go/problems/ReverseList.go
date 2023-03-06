2fe8beb73fe9f51e990e889489e655ba9678c8ad
Sergii Chevkota <chevkota@live.com>
2023-03-06 14:14:49 -0700

    206. Reverse Linked List

--- src/go/problems/ReverseList.go
+++ src/go/problems/ReverseList.go
@@ -2,11 +2,20 @@
 
 package problems
 
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
-
 func ReverseList(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
 
+	var result *ListNode
+	current := head
+
+	for current != nil {
+		next := current.Next
+		current.Next = result
+		result = current
+		current = next
+	}
+
+	return result
 }
