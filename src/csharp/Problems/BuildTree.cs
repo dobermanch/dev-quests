53a083507d1e36ae63078d08d8b658fb756f063f
Sergii Chevkota <chevkota@live.com>
2023-03-12 14:02:57 -0700

    105. Construct Binary Tree from Preorder and Inorder Traversal

--- src/csharp/Problems/BuildTree.cs
+++ src/csharp/Problems/BuildTree.cs
@@ -1,7 +1,5 @@
 //https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
 
-using System;
-
 namespace LeetCode.Problems;
 
 public sealed class BuildTree : ProblemBase
