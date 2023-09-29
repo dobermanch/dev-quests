27d184e01afcb216d70243c86d387c93012b63de
Sergii Chevkota <chevkota@live.com>
2023-09-29 01:29:13 -0700

    added mysql support

---
+++ src/sql/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install MySql commands
+sudo apt update
+sudo apt install -y mysql-server
+sudo service mysql start
+sudo mysql_secure_installation
+
+sudo mysql -u root -p -e "CREATE USER 'leetcode'@'localhost' IDENTIFIED BY 'vscode123'; GRANT ALL PRIVILEGES ON *.* TO 'leetcode'@'localhost' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+# mysql> CREATE USER 'leetcode'@'localhost' IDENTIFIED BY 'vscode123';
+# mysql> GRANT ALL PRIVILEGES ON *.* TO 'leetcode'@'localhost' WITH GRANT OPTION;
+# mysql> FLUSH PRIVILEGES;
+# mysql> exit
+sudo mysql -u leetcode -p -e "CREATE DATABASE leetcode DEFAULT CHARACTER SET = 'utf8mb4'"
