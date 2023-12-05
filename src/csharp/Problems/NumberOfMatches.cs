5902bb42335451e55d8e3c57e2c0c7c45593b079
Sergii Chevkota <chevkota@live.com>
2023-12-04 18:39:18 -0700

    1688. Count of Matches in Tournament

--- src/csharp/Problems/NumberOfMatches.cs
+++ src/csharp/Problems/NumberOfMatches.cs
@@ -19,16 +19,8 @@ public sealed class NumberOfMatches : ProblemBase
 
         while (teams > 1)
         {
-            if (teams % 2 == 0)
-            {
-                teams = teams / 2;
-                matches += teams;
-            }
-            else
-            {
-                teams = (teams - 1) / 2 + 1;
-                matches += teams - 1;
-            }
+            matches += teams / 2;
+            teams = teams / 2 + teams % 2;
         }
 
         return matches;
