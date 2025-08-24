af87da80956d39339da4bc2d0c098b6ccfa336ca
Sergii Chevkota <chevkota@live.com>
2025-08-24 08:33:37 +0000

    fixed tests

--- src/csharp/challenges/Problems/FindSmallestLetterGreaterThanTarget.cs
+++ src/csharp/challenges/Problems/FindSmallestLetterGreaterThanTarget.cs
@@ -9,14 +9,14 @@ public sealed class FindSmallestLetterGreaterThanTarget : ProblemBase
     public override void Test(object[] data) => base.Test(data);
 
     protected override void AddTestCases()
-        => Add(it => it.ParamArray(['c', 'f', 'j']).Add('a').Result('c'))
-          .Add(it => it.ParamArray(['c', 'f', 'j']).Add('f').Result('f'))
-          .Add(it => it.ParamArray(['x','x','y','y']).Add('x').Result('x'))
-          .Add(it => it.ParamArray(['a', 'z']).Add('z').Result('z'))
-          .Add(it => it.ParamArray(['e','e','e','e','e','e','n','n','n','n']).Add('e').Result('n'))
-          .Add(it => it.ParamArray(['c', 'f', 'j']).Add('g').Result('j'))
-          .Add(it => it.ParamArray(['c', 'f', 'j']).Add('d').Result('f'))
-          .Add(it => it.ParamArray(['c','f','j', 'k', 'm', 'p']).Add('l').Result('m'));
+        => Add(it => it.ParamArray(['c', 'f', 'j']).Param('a').Result('c'))
+          .Add(it => it.ParamArray(['c', 'f', 'j']).Param('f').Result('j'))
+          .Add(it => it.ParamArray(['x','x','y','y']).Param('x').Result('y'))
+          .Add(it => it.ParamArray(['a', 'z']).Param('z').Result('a'))
+          .Add(it => it.ParamArray(['e','e','e','e','e','e','n','n','n','n']).Param('e').Result('n'))
+          .Add(it => it.ParamArray(['c', 'f', 'j']).Param('g').Result('j'))
+          .Add(it => it.ParamArray(['c', 'f', 'j']).Param('d').Result('f'))
+          .Add(it => it.ParamArray(['c','f','j', 'k', 'm', 'p']).Param('l').Result('m'));
 
     private char Solution1(char[] letters, char target)
     {
